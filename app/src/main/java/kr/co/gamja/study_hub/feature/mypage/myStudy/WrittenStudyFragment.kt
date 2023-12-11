1e7d0f7fc0822f7adc5bd5542e08b987f3e0ef89
ugyeong <ugyeong76@gmail.com>
2023-12-11 17:23:27 +0900

    [Update] : BottomNav 깜빡임 제거, 작성한 스터디 개수 동기화
    1. bottomNav 깜빡임 제거 코드 추가
    2. 작성한 스터디 개수 관련 api 추가

--- app/src/main/java/kr/co/gamja/study_hub/feature/mypage/myStudy/WrittenStudyFragment.kt
+++ app/src/main/java/kr/co/gamja/study_hub/feature/mypage/myStudy/WrittenStudyFragment.kt
@@ -1,18 +1,16 @@
 package kr.co.gamja.study_hub.feature.mypage.myStudy
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kr.co.gamja.study_hub.R
@@ -21,20 +19,21 @@ import kr.co.gamja.study_hub.data.repository.StudyHubApi
 import kr.co.gamja.study_hub.databinding.FragmentWrittenStudyBinding
 
 class WrittenStudyFragment : Fragment() {
-    private lateinit var binding:FragmentWrittenStudyBinding
-    private lateinit var viewModel : WrittenStudyViewModel
-    private lateinit var writtenStudyAdapter:WrittenStudyAdapter
+    private lateinit var binding: FragmentWrittenStudyBinding
+    private lateinit var viewModel: WrittenStudyViewModel
+    private lateinit var writtenStudyAdapter: WrittenStudyAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-       binding =DataBindingUtil.inflate(inflater,R.layout.fragment_written_study,container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_written_study, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val factory =WrittenStudyViewModelFactory(AuthRetrofitManager.api)
+        val factory = WrittenStudyViewModelFactory(AuthRetrofitManager.api)
         viewModel = ViewModelProvider(this, factory)[WrittenStudyViewModel::class.java]
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
@@ -43,26 +42,31 @@ class WrittenStudyFragment : Fragment() {
         val toolbar = binding.writtenPageToolbar
         (requireActivity() as AppCompatActivity).setSupportActionBar(toolbar)
         (requireActivity() as AppCompatActivity).supportActionBar?.title = ""
-        writtenStudyAdapter=WrittenStudyAdapter(requireContext())
+        writtenStudyAdapter = WrittenStudyAdapter(requireContext())
         setUpRecyclerView()
         observeData()
+        viewModel.updateMyStudyListSize()
     }
-    private fun setUpRecyclerView(){
+
+    private fun setUpRecyclerView() {
         binding.recylerWrittenList.apply {
-            layoutManager=LinearLayoutManager(requireContext())
-            adapter=this@WrittenStudyFragment.writtenStudyAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = this@WrittenStudyFragment.writtenStudyAdapter
         }
     }
-    private fun observeData(){
+
+    private fun observeData() {
         viewLifecycleOwner.lifecycleScope.launch {
-            viewModel.myStudyFlow.collectLatest {
-                pagingData->writtenStudyAdapter.submitData(pagingData) // 데이터 업데이트
+            viewModel.myStudyFlow.collectLatest { pagingData ->
+                writtenStudyAdapter.submitData(pagingData) // 데이터 업데이트
             }
         }
     }
 }
+
 // 생성자 있는 뷰모델 인스턴스 생성
-class WrittenStudyViewModelFactory(private val studyHubApi: StudyHubApi) : ViewModelProvider.Factory {
+class WrittenStudyViewModelFactory(private val studyHubApi: StudyHubApi) :
+    ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(WrittenStudyViewModel::class.java)) { // 타입 확인
             return WrittenStudyViewModel(studyHubApi) as T
