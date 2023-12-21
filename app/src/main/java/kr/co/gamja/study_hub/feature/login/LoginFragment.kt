4018912ad3cdfdd8c7b7ce6703cf6b32c391bc97
uselessnaming <seohojon@naver.com>
2023-12-21 15:45:42 +0900

    fix: 닉네임 공백 입력, 줄바꿈 제한

--- app/src/main/java/kr/co/gamja/study_hub/feature/login/LoginFragment.kt
+++ app/src/main/java/kr/co/gamja/study_hub/feature/login/LoginFragment.kt
@@ -147,7 +147,8 @@ class LoginFragment : Fragment() {
 
         // 회원가입페이지로 연결
         binding.btnRegistration.setOnClickListener {
-            findNavController().navigate(R.id.action_login_to_createAccount, null)
+//            findNavController().navigate(R.id.action_login_to_createAccount, null)
+            findNavController().navigate(R.id.test, null)
         }
 
     }
--- app/src/main/java/kr/co/gamja/study_hub/feature/signup/nickname/NicknameFragment.kt
+++ app/src/main/java/kr/co/gamja/study_hub/feature/signup/nickname/NicknameFragment.kt
@@ -6,10 +6,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.adapters.TextViewBindingAdapter.setText
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -37,6 +40,9 @@ class NicknameFragment : Fragment() {
 
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
+
+        //공백 여부
+        var isSpaced = false
 
         // 에딧텍스트 자판 내리기
         binding.root.setOnTouchListener { v, event ->
@@ -76,12 +82,13 @@ class NicknameFragment : Fragment() {
             binding.btnMale.background = nonSeletedDrawaable
 
             var txt_nickname = binding.editNickName.text.toString()
+
             User.nickname = txt_nickname
             Log.d("회원가입 - User.nickname클릭", User.nickname.toString())
             User.gender = "FEMALE"
             Log.d("회원가입 - 여자", User.gender.toString())
 
-            if (User.nickname != null && User.gender != null && viewModel.nicknameError.value==false) {
+            if (User.nickname != null && User.gender != null && viewModel.nicknameError.value==false && !isSpaced) {
                 binding.btnNext.isEnabled = true
             } else {
                 Log.d("회원가입 - 닉네임이나 성별 null", User.gender.toString() + User.nickname.toString())
@@ -96,7 +103,7 @@ class NicknameFragment : Fragment() {
             Log.d("회원가입 - User.nickname 성별클릭", User.nickname.toString())
             User.gender = "MALE"
             Log.d("회원가입 - 남자", User.gender.toString())
-            if (User.nickname != null && User.gender != null && viewModel.nicknameError.value==false) {
+            if (User.nickname != null && User.gender != null && viewModel.nicknameError.value==false && !isSpaced) {
                 binding.btnNext.isEnabled = true
             } else {
                 Log.d("회원가입 - 닉네임이나 성별 null", User.gender.toString() + User.nickname.toString())
@@ -104,6 +111,7 @@ class NicknameFragment : Fragment() {
         }
 
         binding.btnNext.setOnClickListener {
+            //다른 입력들이 모두 완료되었다면
             findNavController().navigate(
                 R.id.action_createAccountFragment03_to_createAccountFragment04,
                 null
@@ -113,12 +121,32 @@ class NicknameFragment : Fragment() {
         }
         // 닉네임 중복 api
         binding.btnNicknameOverlapCheck.setOnClickListener{
-            viewModel.isDuplicationNickname()
+            //nickname에 공백이 있다면
+            if (checkSpace(binding.editNickName.text.toString()) || binding.editNickName.text!!.isBlank()){
+                val textColor = ContextCompat.getColor(requireContext(), R.color.R_50)
+                binding.errorNickName.visibility = VISIBLE
+                binding.errorNickName.text = "공백이 없는 문자를 입력해주세요"
+                binding.errorNickName.setTextColor(textColor)
+                isSpaced = true
+            } else{
+                viewModel.isDuplicationNickname()
+                isSpaced = false
+            }
         }
         // 닉네임 길이 표시
         viewModel.nickname.observe(viewLifecycleOwner) {
             viewModel.setNicknameLength(it.length) // 닉네임 길이 업뎃
         }
+    }
+
+    //공백이 있는 지 확인하고 있다면 오류 메시지를 넣음
+    private fun checkSpace(text : String) : Boolean{
+        var result = false
+        text.forEach{ ch ->
+            Log.d("AAAAAA","ch : ${ch}")
+            if (ch == ' ') result = true
+        }
+        return result
     }
 
 }
