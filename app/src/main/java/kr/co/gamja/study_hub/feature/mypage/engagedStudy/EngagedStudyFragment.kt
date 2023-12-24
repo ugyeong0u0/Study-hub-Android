dc3b3741355bb3d1c5da67bad252203b0d63edd0
ugyeong <ugyeong76@gmail.com>
2023-12-24 15:48:17 +0900

    [Feat] : 참여한 스터디 UI
    +마이페이지와 북마크페이지연결

---
+++ app/src/main/java/kr/co/gamja/study_hub/feature/mypage/engagedStudy/EngagedStudyFragment.kt
@@ -0,0 +1,40 @@
+package kr.co.gamja.study_hub.feature.mypage.engagedStudy
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import kr.co.gamja.study_hub.R
+import kr.co.gamja.study_hub.databinding.FragmentEngagedStudyBinding
+
+class EngagedStudyFragment : Fragment() {
+    private lateinit var binding: FragmentEngagedStudyBinding
+    private val viewModel: EngagedStudyViewModel by viewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_engaged_study, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        // 툴바 설정
+        val toolbar = binding.engagedStudyToolbar
+        (requireActivity() as AppCompatActivity).setSupportActionBar(toolbar)
+        (requireActivity() as AppCompatActivity).supportActionBar?.title = ""
+
+        binding.iconBack.setOnClickListener {
+            findNavController().navigateUp() // 뒤로 가기
+        }
+    }
+
+}
---
+++ app/src/main/java/kr/co/gamja/study_hub/feature/mypage/engagedStudy/EngagedStudyViewModel.kt
@@ -0,0 +1,18 @@
+package kr.co.gamja.study_hub.feature.mypage.engagedStudy
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class EngagedStudyViewModel :ViewModel() {
+    private val tag = this.javaClass.simpleName
+
+    // 리스트 개수
+    private val _listSize = MutableLiveData<Int>()
+    val listSize: LiveData<Int> get() = _listSize
+
+    // 참여 스터디 리스트 총 개수 가져오기
+    fun getEngagedStudy(){
+
+    }
+}
