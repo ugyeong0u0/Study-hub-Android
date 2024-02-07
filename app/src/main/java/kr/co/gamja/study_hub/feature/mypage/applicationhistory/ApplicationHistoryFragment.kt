2e345f3b8114923eca0558d7dd4d48d425a685ad
ugyeong <ugyeong76@gmail.com>
2024-02-08 01:28:49 +0900

    [Feat] : 신청내역 UI

---
+++ app/src/main/java/kr/co/gamja/study_hub/feature/mypage/applicationhistory/ApplicationHistoryFragment.kt
@@ -0,0 +1,41 @@
+package kr.co.gamja.study_hub.feature.mypage.applicationhistory
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import kr.co.gamja.study_hub.R
+import kr.co.gamja.study_hub.databinding.FragmentApplicationHistoryBinding
+
+class ApplicationHistoryFragment : Fragment() {
+    private lateinit var binding: FragmentApplicationHistoryBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_application_history,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // 툴바 설정
+        val toolbar = binding.applicationHistoryToolbar
+        (requireActivity() as AppCompatActivity).setSupportActionBar(toolbar)
+        (requireActivity() as AppCompatActivity).supportActionBar?.title = ""
+
+        binding.iconBack.setOnClickListener {
+            findNavController().navigateUp() // 뒤로 가기
+        }
+    }
+
+}
